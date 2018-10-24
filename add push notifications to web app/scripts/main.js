0e2909b138d8c89b23edafadf336b9a5870b21da
ajafik <oluwafikayoajayi@gmail.com>
2018-10-24 05:55:22 +0100

    added user unsubscription from push notification

--- add push notifications to web app/scripts/main.js
+++ add push notifications to web app/scripts/main.js
@@ -68,7 +68,7 @@ function initializeUI() {
   pushButton.addEventListener('click', function() {
     pushButton.disabled = true;
     if (isSubscribed) {
-      // TODO: Unsubscribe user
+      unsubscribeUser();
     } else {
       subscribeUser();
     }
@@ -95,7 +95,7 @@ function updateBtn() {
     updateSubscriptionOnServer(null);
     return;
   }
-  
+
   if (isSubscribed) {
     pushButton.textContent = 'Disable Push Messaging';
   } else {
@@ -140,4 +140,24 @@ function updateSubscriptionOnServer(subscription) {
   } else {
     subscriptionDetails.classList.add('is-invisible');
   }
+}
+
+function unsubscribeUser() {
+  swRegistration.pushManager.getSubscription()
+  .then(function(subscription) {
+    if (subscription) {
+      return subscription.unsubscribe();
+    }
+  })
+  .catch(function(error) {
+    console.log('Error unsubscribing', error);
+  })
+  .then(function() {
+    updateSubscriptionOnServer(null);
+
+    console.log('User is unsubscribed.');
+    isSubscribed = false;
+
+    updateBtn();
+  });
 }
